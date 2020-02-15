4a9fd10255ec3b86cd02760e161f6e931af563b1
Andrew Aguirre <seesharpprojects@hotmail.com>
2020-02-15 17:41:09 +0800

    Full setup of template Jan 2020

---
+++ ReactOnNet/Scripts/React/webpack.config.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../app.js" />
+module.exports = {
+    watch: true,
+    mode: "development",
+    context: __dirname,
+    entry: "./app.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    }
+}
+
