0fe6d11c5a6c2d5a42af529e153a14830c2ac469
Andrew Aguirre <seesharpprojects@hotmail.com>
2020-02-15 19:53:52 +0800

    Add new components

--- ReactOnNet/Scripts/React/app.js
+++ ReactOnNet/Scripts/React/app.js
@@ -1,7 +1,49 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
+ 
+//ReactDOM.render(
+//    <h1>Hello, {document.getElementById('txtName').value}</h1>,
+//    document.getElementById('root')
+//);
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-);
+
+class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">Hello, world! I am a CommentBox.</div>
+        );
+    }
+}
+
+ReactDOM.render(<CommentBox />, document.getElementById('root'));
